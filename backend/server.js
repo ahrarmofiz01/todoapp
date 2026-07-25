d26fa15295f4489afe281463ef7bcd47a95e3950
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-25 13:53:40 +0530

    add code

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Connected");
+})
+.catch(err=>{
+    console.log(err);
 });
 
-app.get("/todos", (req, res) => {
-    res.json([]);
-});
+app.use("/todos", require("./routes/todoRoutes"));
 
-app.listen(5000, () => {
-    console.log("Server running");
+const PORT = 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${PORT}`);
 });
